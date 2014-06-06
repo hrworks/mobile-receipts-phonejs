7293fa737ef77097c227a2d15474562dcef64e93
christoph-hummel <christoph.hummel@hrworks.de>
2014-06-06 17:01:18 +0200

    test

--- index.js
+++ index.js
@@ -203,7 +203,7 @@ $(function () {
 	HRworksReceipt.app.router.register(":view/:id", { view: "home", id: undefined });
 	HRworksReceipt.app.navigate();
 	
-
+	document.addEventListener("deviceready", function() { navigator.splashscreen.hide(); });
 });
 	   function onViewShown(args) {
         var viewInfo = args.viewInfo;

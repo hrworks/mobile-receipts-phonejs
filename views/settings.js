c5c7a5b9f6ec0b39189bd75c3223aca9b8a344a8
christoph-hummel <christoph.hummel@hrworks.de>
2014-06-06 14:28:50 +0200

    100 Example Receipts

--- views/settings.js
+++ views/settings.js
@@ -1,5 +1,18 @@
 ﻿HRworksReceipt.settings = function (params) {
 	var viewModel = {
+		exampleReceipts: function() {
+			for(var i= 0; i < 100; i++) {
+				HRworksReceipt.localStoreReceipts.insert({
+					text : 'Receipt' + i,
+					amount : '111',
+					date : '20120304',
+					receiptKind : '1',
+					kindOfPayment : '1',
+					currency : 'EUR',
+					timestamp : Date()
+				});
+			}
+		},
 		serverSource : [{
 				name : "Produktiver Server"
 			}, {
